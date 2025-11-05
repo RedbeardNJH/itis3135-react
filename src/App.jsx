91da2a27bab1f8eada58d19f101279c1c8aee57f
RedbeardNJH <154090043+RedbeardNJH@users.noreply.github.com>
2025-11-05 00:29:35 -0500

    small update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
       <main>
       <h2>ITIS 3135 Homepage</h2>
       <p>This is all the content for my ITIS 3135 class</p>
-      <p>BOMBS</p>
       <p>
         <a href="nocturnal_hampster_pmc/about.html">Nocturnal Hamster PMC</a> ||&nbsp;
         <a href="stuff/Absolute%20Bangin'%20Webpage.htm">Absolute Bangin' Webpage</a> ||&nbsp;
